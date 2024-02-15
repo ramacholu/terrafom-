ef6e94dbaf90ce13d6ccf7ffad0d213b10d06308
Ubuntu <ubuntu@ip-172-31-87-73.ec2.internal>
2024-02-15 22:02:55 +0000

    this is the file for terraform ec2

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0c7217cdde317cfec"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+tags = {
+Name = "terra"
+}
+}
